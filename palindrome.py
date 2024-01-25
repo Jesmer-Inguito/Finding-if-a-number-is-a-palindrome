b6f7aa700fc63e172204c9261e1b6ab72cab9720
unknown <you@example.com>
2024-01-25 11:59:23 +0800

    pseudocode

---
+++ palindrome.py
@@ -0,0 +1,9 @@
+# pseudocode
+
+# Create a function that will determine if the number is the same if reversed
+
+# User input a number
+
+# Lock the input into interger only
+
+# if-else statement that will call the function / results
