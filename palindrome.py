6fa845ad0e415bb45de8086399bbfbdd73bd8bd4
unknown <you@example.com>
2024-01-25 12:01:53 +0800

    function done

--- palindrome.py
+++ palindrome.py
@@ -1,6 +1,11 @@
 # pseudocode
 
 # Create a function that will determine if the number is the same if reversed
+def palindrome(number):
+    number_str = str(number)
+
+    reversed_str = number_str[::-1]
+    return number_str == reversed_str
 
 # User input a number
 
